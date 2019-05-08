63277f68048cdf7b4eabbb50419a0278821646cb
raquelroldo <raquelroldo@gmail.com>
2019-05-08 11:51:15 +0100

    Delete folder and files unused

--- giraffe-course/first_python-20190429T142112Z-001/first_python/inheritance.py
+++
@@ -1,8 +0,0 @@
-from chef import Chef
-from chinese_chef import ChineseChef
-
-myChef = Chef()
-myChef.makes_chicken()
-
-myChineseChef = ChineseChef()
-myChineseChef.makes_special_dish()
--- giraffe-course/first_python-20190429T142112Z-001/first_python/modules_pip.py
+++
@@ -1,5 +0,0 @@
-#list of Python Modules on Google
-#pip package manager
-import forLoop
-
-print(forLoop.friends())
