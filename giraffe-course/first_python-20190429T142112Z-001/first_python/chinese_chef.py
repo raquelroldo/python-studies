e9a6c5d6a26ef036ee3cadf9b40f07809f889318
raquelroldo <raquelroldo@gmail.com>
2019-05-08 11:52:59 +0100

    delete folder

--- giraffe-course/first_python-20190429T142112Z-001/first_python/chinese_chef.py
+++
@@ -1,6 +0,0 @@
-from chef import Chef
-class ChineseChef(Chef):
-    def makes_special_dish(self):
-        print("Chef makes orange chicken")
-    def makes_fried_rice(self):
-        print("Chef makes fried rice")
